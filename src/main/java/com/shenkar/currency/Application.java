7c6bc9a9144593f1746d5ac688f01b84a9ebd98b
Edan Hauon <edanhauon@gmail.com>
2017-08-05 14:04:05 +0300

    merge via intellij

--- src/main/java/com/shenkar/currency/Application.java
+++ src/main/java/com/shenkar/currency/Application.java
@@ -1,7 +1,6 @@
 package com.shenkar.currency;
 
 
-
 public class Application {
     public static void main(String[] args) throws Throwable{
         CurrencyDataHolder currencyDataHolder = new CurrencyDataHolder();
